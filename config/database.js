e88c311adef402f9849fb63947eb8ea026b65760
Rucha Wankhede <rucha_wankhede@ildc-inlp.idealake.com>
2020-03-16 11:47:30 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+//Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost:27017/noderestapi';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+module.exports = mongoose;
+
+// connection to mongodb
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
